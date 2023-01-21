edea8bc5d83e027234f2b186ae137cc119ce9599
berkmahim <berkmahim620@gmail.com>
2023-01-22 01:01:59 +0300

    static files aded

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 import express from 'express'
+import path from 'path'
+const __dirname = path.resolve();
+
+
 
 const app = express()
 const port = 3000
 
+
+
+app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
-    res.send('helreo wsorldort')
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
 })
 
 
