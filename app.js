4ba5d8b8de6a388c463db24bad9f91fa588d32e2
berkmahim <berkmahim620@gmail.com>
2023-01-21 21:37:38 +0300

    nodemon added

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/',(req,res) => {
-    res.send('hello world')
+app.get('/', (req, res) => {
+    res.send('helreo wsorldort')
 })
 
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log(`sunucu ${port} portunda başlatıldı`)
 })
