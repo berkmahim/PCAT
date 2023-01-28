367beae71e09e310ee446ad30cba0a8a23f4170c
berkmahim <berkmahim620@gmail.com>
2023-01-28 16:48:02 +0300

    Ejs added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import path from 'path'
+import ejs from 'ejs'
 const __dirname = path.resolve();
 
 
 
 const app = express()
+app.set('view engine', 'ejs')
 const port = 3000
 
 
@@ -13,7 +15,13 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+    res.render('index')
+})
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/add', (req, res) => {
+    res.render('add')
 })
 
 
